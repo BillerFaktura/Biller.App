9b60dbe1062f21b9c92bc94955deb9f588161ba0
LastElb <igor.lueckel@googlemail.com>
2014-04-10 12:56:27 +0200

    Optimized Percentage XML-output

--- Biller.Data/Articles/OrderedArticle.cs
+++ Biller.Data/Articles/OrderedArticle.cs
@@ -238,7 +238,7 @@ namespace Biller.Data.Articles
             OrderedAmount = double.Parse(source.Element("OrderedAmount").Value, CultureInfo.InvariantCulture);
             OrderPosition = int.Parse(source.Element("ArticlePosition").Value);
             OrderPrice.ParseFromXElement(source.Element("OrderPrice").Element("PriceGroup"));
-            OrderRebate.ParseFromXElement(source.Element("OrderRebate"));       
+            OrderRebate.ParseFromXElement(source.Element(OrderRebate.XElementName));       
         }
 
         /// <summary>
@@ -250,7 +250,7 @@ namespace Biller.Data.Articles
             return new XElement(XElementName, new XElement("ArticleID", ArticleID), new XElement("ArticleDescription", ArticleDescription),
                          new XElement("OrderText", OrderText), new XElement("OrderedAmount", OrderedAmount),
                          new XElement("ArticlePosition", OrderPosition), new XElement("OrderPrice", OrderPrice.GetXElement()),
-                         new XElement("OrderRebate", OrderRebate.GetXElement()), new XElement("ArticleWeight", ArticleWeight),
+                         new XElement(OrderRebate.GetXElement()), new XElement("ArticleWeight", ArticleWeight),
                          new XElement("ArticleUnit", ArticleUnit.Name), new XElement("TaxClass", TaxClass.Name));
         }
 
